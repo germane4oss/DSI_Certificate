d9d73827e82477195f928c7056d7b9fccd957a9d
German Silva <german@e4oss.ca>
2023-12-09 06:31:13 -0500

    Assignmet for Week 1

--- extract_data.sh
+++ extract_data.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-
-#################################################################################################
-# This script receives a filename a column index and a filter, it extracts the data from the file
-# and applies the filter.
-#################################################################################################
+###########################################################################
+# Shellscript:	extract_data.sh - the script receives a file, an Index and a filter
+#               and prints the data that matches the filter (Bourne Shell)
+# Author     :	German Silva (german@e4oss.ca)
+# Date       :	12-05-23
+# Category   :	DSI - Homework
+###########################################################################
+# Description
+#
+# Changes
+# 
+###########################################################################
 
 filename=$1
 column=$2
 filter=$3
+
+#### FUNCTION filterData
+# Receives a String and checks if it matches the positional filter Argument 
+# GLOBALS: 
+# 	None
+# ARGUMENTS: 
+# 	Name as a String to use as filter
+# OUTPUTS: 
+# 	None
+# RETURN: 
+# 	the string if there is a match if, nothing otherwise.
+### FUNCTION END
 
 function filterData {
     local filter=$1
